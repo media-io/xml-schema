6623691f7eddbd2e0b65d67d2015e66584988e67
Andrei <matzipan@gmail.com>
2023-12-24 21:57:32 +0000

    Add tests for group

--- xml_schema_derive/src/xsd/group.rs
+++ xml_schema_derive/src/xsd/group.rs
@@ -59,3 +59,50 @@ impl Group {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use yaserde::de::from_str;
+
+  #[test]
+  fn check_group_implementation() {
+    let xml = r#"
+        <group name="groupthing">
+          <sequence>
+            <element name="CX_X" type="asdfg"/>
+            <element name="CY_X" type="asdfg"/>
+          </sequence>
+        </group>
+    "#;
+
+    let group: Group = from_str(xml).unwrap();
+
+    let context =
+      XsdContext::new(r#"<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"></xs:schema>"#)
+        .unwrap();
+
+    let implementation = format!("{}", group.implement(&TokenStream::new(), &None, &context));
+
+    assert_eq!(implementation, "# [derive (Clone , Debug , Default , PartialEq , serde :: Deserialize , serde :: Serialize)] \
+pub struct Groupthing { \
+# [yaserde (rename = \"CX_X\")] pub cx_x : xml_schema_types :: Asdfg , \
+# [yaserde (rename = \"CY_X\")] pub cy_x : xml_schema_types :: Asdfg , }");
+  }
+
+  #[test]
+  fn check_group_ref() {
+    let xml = r#"<group ref="bla:groupthing" />"#;
+
+    let group: Group = from_str(xml).unwrap();
+
+    let context =
+      XsdContext::new(r#"<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"></xs:schema>"#)
+        .unwrap();
+
+    let type_implementation = format!("{}", group.get_type_implementation(&context, &None));
+
+    assert_eq!(type_implementation, "Groupthing");
+  }
+}
